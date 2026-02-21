96da861efe5ce2f9b6eca264df92374f0ce1a522
Sk-Azraf-Sami <azraf7026official@gmail.com>
2026-02-21 12:53:47 +0600

    fix: hot reload port bind issue

--- codes/src/main.ts
+++ codes/src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.CONTAINER_PORT ?? 5000);
+
+  const port = Number(process.env.CONTAINER_PORT ?? process.env.PORT ?? 5000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
